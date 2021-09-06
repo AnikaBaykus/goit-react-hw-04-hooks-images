797b598f708e08ec35bac97da886ae303b4f815c
Anna_Matveeva <75207035+AnikaBaykus@users.noreply.github.com>
2021-09-06 20:58:28 +0300

    Updates

--- static/js/main.4a043bf5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonphw-04-hooks-images"]=this["webpackJsonphw-04-hooks-images"]||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var s=t(1),c=t.n(s),o=t(3),i=t.n(o),r=(t(8),t(9),t(0));var a=function(){return Object(r.jsx)("div",{className:"App",children:"ALOHA"})};i.a.render(Object(r.jsx)(c.a.StrictMode,{children:Object(r.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.4a043bf5.chunk.js.map
